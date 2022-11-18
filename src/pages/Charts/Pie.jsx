0d0ef8b2df8c534ea99f2518f9ef3ff19359019a
SaarthakTuli <saarthaktuli@gmail.com>
2022-11-19 02:32:31 +0530

    init

--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
+import { Header } from '../../components';
+
+import { useStateContext } from '../../contexts/ContextProvider';
+
+import { PieChart } from '../../components';
+
 const Pie = () => {
   return (
-    <div>Pie</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header
+        category='Pie'
+        title='Project Cost Breakdown'
+      />
+
+      <div className='w-full'>
+        <PieChart />
+      </div>
+    </div>
   )
 }
 
